993bbe3708c8d093c258cc2da0c4c6c3e13c4cbd
Sundaram singh <155410013+Sundaramsingh21@users.noreply.github.com>
2024-12-22 16:39:54 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -31,7 +31,7 @@ app.use(cors({
     }
   },
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  allowedHeaders: ["Content-Type", "Authorization","token"],
 }));
 
 //db connection
