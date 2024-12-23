343ed29c5b4d090f67451f7b6f84c9fe288c753f
Sundaram singh <155410013+Sundaramsingh21@users.noreply.github.com>
2024-12-23 14:25:33 +0530

    Update componentRoutes.js

--- Backend/routes/componentRoutes.js
+++ Backend/routes/componentRoutes.js
@@ -1,17 +1,31 @@
 import express from "express"
 import { addComponent ,CompInfo,listComponent ,removecomponent, updateprice, updateStock} from "../controllers/componentController.js"
 import multer from "multer"
+import fs from "fs";
+import path from "path";
 
 const componentRouter = express.Router();
 
 //Image Storage Engine
 
+// const storage = multer.diskStorage({
+//     destination: "uploads",
+//     filename:(req,file,cb)=>{
+//         return cb(null,`${Date.now()}${file.originalname}`)
+//     }
+// })
 const storage = multer.diskStorage({
-    destination: "uploads",
-    filename:(req,file,cb)=>{
-        return cb(null,`${Date.now()}${file.originalname}`)
-    }
-})
+    destination: (req, file, cb) => {
+        const dir = path.join(process.cwd(), "uploads");
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir, { recursive: true });
+        }
+        cb(null, dir);
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now()}-${file.originalname}`);
+    },
+});
 
 const upload = multer({storage:storage})
 
