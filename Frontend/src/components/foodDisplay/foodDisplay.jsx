1c75a63361aa67e435fff7f680bb92bf3a6dfa21
Sundaram singh <155410013+Sundaramsingh21@users.noreply.github.com>
2024-12-24 20:25:03 +0530

    Update foodDisplay.jsx

--- Frontend/src/components/foodDisplay/foodDisplay.jsx
+++ Frontend/src/components/foodDisplay/foodDisplay.jsx
@@ -2,13 +2,14 @@ import React, { useContext } from 'react'
 import './foodDisplay.css'
 import { StoreContext } from '../../Context/StoreContext'
 import CompItem from '../CompItem/CompItem'
-import Loader from '../../../../admin/src/components/Loader/Loader'
+import FrontendLoader from '../FrontendLoader/FrontendLoader'
+
 const foodDisplay = ({ category }) => {
   const { food_list ,LoaderF} = useContext(StoreContext)
   return (
     <div className='Item-display' id='Item-display'>
       <h2>Top Items</h2>
-      <center>{LoaderF?<Loader/>:<></>}</center>
+      <center>{LoaderF?<FrontendLoader/>:<></>}</center>
       <div className="Item-display-list">
         {food_list.map((item, index) => {
           if (category === "All" || category === item.category) {
