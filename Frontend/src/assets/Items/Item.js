a1443125fedb99992262c8ad67bc0e24459f33e6
Sundaram singh <155410013+Sundaramsingh21@users.noreply.github.com>
2024-12-25 16:25:34 +0530

    Update Item.js

--- Frontend/src/assets/Items/Item.js
+++ Frontend/src/assets/Items/Item.js
@@ -2,6 +2,6 @@ import green_LED from '../Items/1734948073607-green.png'
 import red_LED from '../Items/1735123682900LED_Red_LED.png'
 
 export const Items ={
-    '1734948073607-green.png' : green_LED
+    '1734948073607-green.png' : green_LED,
     '1735123682900LED_Red_LED.png' : red_LED
 }
