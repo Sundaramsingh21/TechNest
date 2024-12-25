d3bf3e75c48e71e8382269f6aaa6cb655a997024
Sundaram singh <155410013+Sundaramsingh21@users.noreply.github.com>
2024-12-25 20:48:47 +0530

    Add files via upload

---
+++ Frontend/src/assets/Items/Item.js
@@ -0,0 +1,31 @@
+import green_LED from '../Items/LED/1734948073607-green.png'
+import red_LED from '../Items/LED/1735123682900LED_Red_LED.png'
+
+
+//Capacitor
+import capacitor_47uf_25v from '../Items/Capacitors/173512842802047uF-25V.png'
+import capacitor_1uf_63v from '../Items/Capacitors/17351284721061uF-63V.png'
+import capacitor_4uF_50V from '../Items/Capacitors/17351285346224.7uF-50V-Electrolyti.png'
+import capacitor_4uF_450V from '../Items/Capacitors/17351285824584-7uF-450V.png'
+import capacitor_25uF from '../Items/Capacitors/173512863554322uF-Radial-Electrolytic.png'
+import capacitor_47000pf from '../Items/Capacitors/173512874147547000pF-Ceramic-Capacitor.png'
+import capacitor_22000pf from '../Items/Capacitors/173512879122122000pF-Ceramic.png'
+import capacitor_470uf from '../Items/Capacitors/1735128864494470uF-50V.png'
+
+
+export const Items ={
+    //LED
+    '1734948073607-green.png' : green_LED,
+    '1735123682900LED_Red_LED.png': red_LED,
+
+    //Capacitor
+    '173512842802047uF-25V.png' : capacitor_47uf_25v,
+    '17351284721061uF-63V.png' : capacitor_1uf_63v,
+    '17351285346224.7uF-50V-Electrolyti.png' : capacitor_4uF_50V,
+    '17351285824584-7uF-450V.png' :capacitor_4uF_450V,
+    '173512863554322uF-Radial-Electrolytic.png' :capacitor_25uF,
+    '173512874147547000pF-Ceramic-Capacitor.png' : capacitor_47000pf,
+    '173512879122122000pF-Ceramic.png' : capacitor_22000pf,
+    '1735128864494470uF-50V.png' : capacitor_470uf
+
+}
