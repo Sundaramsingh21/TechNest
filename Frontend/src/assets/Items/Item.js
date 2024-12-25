be9b77cbabfbc8a2df0a3cf1f284583964a06ff9
Sundaram singh <155410013+Sundaramsingh21@users.noreply.github.com>
2024-12-25 20:47:58 +0530

    Delete Frontend/src/assets/Items directory

--- Frontend/src/assets/Items/Item.js
+++
@@ -1,7 +0,0 @@
-import green_LED from '../Items/1734948073607-green.png'
-import red_LED from '../Items/1735123682900LED_Red_LED.png'
-
-export const Items ={
-    '1734948073607-green.png' : green_LED,
-    '1735123682900LED_Red_LED.png' : red_LED
-}
