a74dd31c436a6c093cb421f1379092a751c53ff4
Sundaram singh <155410013+Sundaramsingh21@users.noreply.github.com>
2024-12-27 10:56:30 +0530

    Update Item.js

--- Frontend/src/assets/Items/Item.js
+++ Frontend/src/assets/Items/Item.js
@@ -1,5 +1,9 @@
 import green_LED from '../Items/LED/1734948073607-green.png'
 import red_LED from '../Items/LED/1735123682900LED_Red_LED.png'
+import LED_yellow from '../Items/LED/17352710484115mm-LED-Yellow-Color_1024x1024@2x.webp'
+import LED_orange from '../Items/LED/17352711500133mm-LED-Orange_1024x1024.webp'
+import LED_photodiode_reciever from '../Items/LED/1735272097163Photodiode_1_1024x1024@2x.webp'
+import LED_photodiode_transmitter from '../Items/LED/17352721574875mm-IR-Transmitter-LED_1024x1024@2x.webp'
 
 
 //Capacitor
@@ -12,11 +16,30 @@ import capacitor_47000pf from '../Items/Capacitors/173512874147547000pF-Ceramic-
 import capacitor_22000pf from '../Items/Capacitors/173512879122122000pF-Ceramic.png'
 import capacitor_470uf from '../Items/Capacitors/1735128864494470uF-50V.png'
 
+//Resistros
+import Resistor10ohm_resistor from '../Items/Resistors/173527165639410ohm-resistor_1024x1024.png'
+import Resistor33ohm_resistor from '../Items/Resistors/173527196532833ohm-resistor_1024x1024.png'
+import Resistor33kohm_resistor from '../Items/Resistors/173527242588533kohm-Resistor_1024x1024@2x.webp'
+import Resistor270ohm_resistor from '../Items/Resistors/1735273596264270-ohm-Resistor_1024x1024@2x.png'
+import Resistor2point2ohm_resistor from '../Items/Resistors/17352736645912.2-ohm-14-Watt-Resistor_1024x1024@2x.png'
+import Resistor1_5Mohm_resistor from '../Items/Resistors/17352737705531.5M-ohm-Resistor_1024x1024@2x.webp'
+import Resistor560kohm_resistor from '../Items/Resistors/1735273860241560K-ohm-Resistor_1024x1024.webp'
+import Resistor82ohm_resistor from '../Items/Resistors/173527401912882ohm-Resistor_1024x1024.webp'
+import Resistor8_2kohm_resistor from '../Items/Resistors/17352741098198.2kohm-Resistor_1024x1024@2x.webp'
 
 export const Items ={
     //LED
     '1734948073607-green.png' : green_LED,
     '1735123682900LED_Red_LED.png': red_LED,
+    '17352710484115mm-LED-Yellow-Color_1024x1024@2x.webp':LED_yellow,
+    '17352711500133mm-LED-Orange_1024x1024.webp': LED_orange,
+    '17352711986823mm-LED-Red_1024x1024.webp' : red_LED,
+    '17352712227453mm-led-green_1024x1024.webp': green_LED,
+    '17352713818025mm-led-white_1024x1024.webp' : LED_orange,
+    '1735272097163Photodiode_1_1024x1024@2x.webp': LED_photodiode_reciever,
+    '17352721574875mm-IR-Transmitter-LED_1024x1024@2x.webp': LED_photodiode_transmitter,
+    '1735272255936Photodiode_1024x1024.webp': LED_photodiode_reciever,
+    '17352723519193mm-IR-Transmitter-LED_1024x1024@2x.png' : LED_photodiode_transmitter,
 
     //Capacitor
     '173512842802047uF-25V.png' : capacitor_47uf_25v,
@@ -26,6 +49,23 @@ export const Items ={
     '173512863554322uF-Radial-Electrolytic.png' :capacitor_25uF,
     '173512874147547000pF-Ceramic-Capacitor.png' : capacitor_47000pf,
     '173512879122122000pF-Ceramic.png' : capacitor_22000pf,
-    '1735128864494470uF-50V.png' : capacitor_470uf
+    '1735128864494470uF-50V.png' : capacitor_470uf,
+
+    //Resistors
+    '173527165639410ohm-resistor_1024x1024.png' :  Resistor10ohm_resistor,
+    '1735271812747680ohm-Resistor_1024x1024@2x.png' : Resistor10ohm_resistor,
+    '173527196532833ohm-resistor_1024x1024.png' : Resistor33ohm_resistor,
+    '173527242588533kohm-Resistor_1024x1024@2x.webp' : Resistor33kohm_resistor,
+    '17352725140873.3kohm-Resistor_1024x1024.webp' : Resistor10ohm_resistor,
+    '1735273596264270-ohm-Resistor_1024x1024@2x.png' : Resistor270ohm_resistor,
+    '17352736645912.2-ohm-14-Watt-Resistor_1024x1024@2x.png' : Resistor2point2ohm_resistor,
+    '1735273713698560ohm-Resistor_1024x1024@2x.webp' : Resistor10ohm_resistor,
+    '17352737705531.5M-ohm-Resistor_1024x1024@2x.webp': Resistor1_5Mohm_resistor,
+    '1735273860241560K-ohm-Resistor_1024x1024.webp' : Resistor560kohm_resistor,
+    '1735273907186270K-ohm-Resistor_1024x1024@2x.webp' :  Resistor33kohm_resistor,
+    '17352739617822kohm-Resistor_1024x1024@2x.webp' :  Resistor10ohm_resistor,
+    '173527401912882ohm-Resistor_1024x1024.webp' :Resistor82ohm_resistor,
+    '173527406564282K-ohm-Resistor_1024x1024@2x.webp' : Resistor33kohm_resistor,
+    '17352741098198.2kohm-Resistor_1024x1024@2x.webp' :Resistor8_2kohm_resistor
 
 }
