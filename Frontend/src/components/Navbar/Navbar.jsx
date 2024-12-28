341ae0a7b35b6762e43d5e198844ee5c9215e141
Sundaram singh <155410013+Sundaramsingh21@users.noreply.github.com>
2024-12-28 13:48:58 +0530

    Update Navbar.jsx

--- Frontend/src/components/Navbar/Navbar.jsx
+++ Frontend/src/components/Navbar/Navbar.jsx
@@ -1,18 +1,31 @@
-import React, { Profiler, useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import './Navbar.css'
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom'
 import { assets } from '../../assets/assets'
 import { StoreContext } from '../../Context/StoreContext';
 
-const Navbar = ({ setShowLogin }) => {
+const Navbar = ({ setShowLogin, setsearchquery, searchquery }) => {
     const [menu, setmenu] = useState("Home")
+    const [showSearch, setshowSearch] = useState(false)
 
     const { getTotalCartAmount, Token, setToken } = useContext(StoreContext)
 
     const navigate = useNavigate();
 
-    const Logout= ()=>{
+    const handlechange = (e) => {
+        const query = e.target.value;
+        setsearchquery(query)
+    }
+    const closesearch = () => {
+        setsearchquery('')
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        });
+        setshowSearch(false)
+    }
+    const Logout = () => {
         localStorage.removeItem("Token");
         setToken("");
         navigate("/");
@@ -21,36 +34,43 @@ const Navbar = ({ setShowLogin }) => {
     return (
         <div className='navbar'>
             <Link to="/"><img className="logo" src={assets.logo} alt="" /></Link>
-            {/* <Link to="/"><div className="logo">AS-India</div></Link> */}
-            <ul className='navbar-menu'>
-                <Link to='/' onClick={() => { setmenu("Home") }} className={menu === "Home" ? "active" : ""}>Home</Link>
-                <Link to='/myorders' onClick={() => { setmenu("Menu") }} className={menu === "Menu" ? "active" : ""}>Orders</Link>
-                <a href='#footer' onClick={() => { setmenu("Contact us") }} className={menu === "Contact us" ? "active" : ""}>Contact us</a>
-                <a href='#footer' onClick={() => { setmenu("About") }} className={menu === "About" ? "active" : ""}>About</a>
-            </ul>
+            {showSearch ? <>
+                <div className="search-icon-cross">
+                    <input name='search' value={searchquery} onChange={handlechange} type="text" placeholder='Search' />
+                    <p id='search-deasble-button' onClick={closesearch} >X</p>
+                </div>
+            </> :
+                <ul className='navbar-menu'>
+                    <Link to='/' onClick={() => { setmenu("Home") }} className={menu === "Home" ? "active" : ""}>Home</Link>
+                    <Link to='/myorders' onClick={() => { setmenu("Menu") }} className={menu === "Menu" ? "active" : ""}>Orders</Link>
+                    <a href='#footer' onClick={() => { setmenu("Contact us") }} className={menu === "Contact us" ? "active" : ""}>Contact us</a>
+                    <a href='#footer' onClick={() => { setmenu("About") }} className={menu === "About" ? "active" : ""}>About</a>
+                </ul>}
+
             <div className="navbar-right">
-                
+                {showSearch ? <></> : <img onClick={() => setshowSearch(true)} src={assets.search_icon} alt="" />}
                 <div className="navbar-search-icon">
-                    {/* <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link> */}
+
                     <Link to='/cart'>
-                        <lord-icon 
+                        <lord-icon
                             src="https://cdn.lordicon.com/pbrgppbb.json"
                             trigger="hover"
                             colors="primary:#49557e"
-                            >
+                        >
                         </lord-icon>
                     </Link>
                     <div className={getTotalCartAmount() === 0 ? "" : "dot"}></div>
 
                 </div>
-                {!Token?<button onClick={() => setShowLogin(true)}>Sign in</button> : <div className='navbar-Profile'>
-                    <img className='profile' src={assets.profile_icon} alt="" />
-                    <ul className="nav-profile-dropdown">
-                        <Link to='/myorders'><li><img src={assets.bag_icon} alt="" /><p>Orders</p></li></Link>
-                        <hr />
-                        <li onClick={Logout} ><img src={assets.logout_icon} alt="" />Logout</li>
-                    </ul>
-                </div>}
+                {!Token ? <button onClick={() => setShowLogin(true)}>Sign in</button> :
+                    <div className='navbar-Profile'>
+                        <img className='profile' src={assets.profile_icon} alt="" />
+                        <ul className="nav-profile-dropdown">
+                            <Link to='/myorders'><li><img src={assets.bag_icon} alt="" /><p>Orders</p></li></Link>
+                            <hr />
+                            <li onClick={Logout} ><img src={assets.logout_icon} alt="" />Logout</li>
+                        </ul>
+                    </div>}
 
             </div>
 
