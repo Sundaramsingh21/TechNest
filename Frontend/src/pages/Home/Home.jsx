3ac7a58c73efc13e63603885c6e1a858ddf3aa3e
Sundaram singh <155410013+Sundaramsingh21@users.noreply.github.com>
2024-12-28 13:52:30 +0530

    Update Home.jsx

--- Frontend/src/pages/Home/Home.jsx
+++ Frontend/src/pages/Home/Home.jsx
@@ -1,19 +1,29 @@
 import React from 'react'
 import './Home.css'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
-// import ItemDisplay from '../../components/foodDisplay/foodDisplay'
 import FoodDisplay from '../../components/foodDisplay/foodDisplay'
-const Home = () => {
+const Home = ({ searchquery }) => {
   const [category, setcategory] = useState("All")
+  useEffect(() => {
+    if (searchquery.length > 0 ) {
+      const isMobile = window.innerWidth <= 500; 
+      const scrollTop = isMobile ? 415 : 830; 
+  
+      window.scrollTo({
+        top: scrollTop,
+        behavior: "smooth",
+      });
+    }
+  }, [searchquery]);
+
   return (
     <div>
-      <Header/>
-      <ExploreMenu category={category} setcategory={setcategory}/>
-       {/* <ItemDisplay category={category}/> */}
-       <FoodDisplay category={category}/>
-       
+      <Header />
+      <ExploreMenu category={category} setcategory={setcategory} />
+      <FoodDisplay category={category} searchquery={searchquery} />
+
     </div>
   )
 }
