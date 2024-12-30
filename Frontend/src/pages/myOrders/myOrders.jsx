f342e43e2d35fb749652d92d0d4412e50297397c
Sundaram singh <155410013+Sundaramsingh21@users.noreply.github.com>
2024-12-30 16:19:25 +0530

    Update myOrders.jsx

--- Frontend/src/pages/myOrders/myOrders.jsx
+++ Frontend/src/pages/myOrders/myOrders.jsx
@@ -1,60 +1,70 @@
-import React ,{useState,useContext,useEffect}from 'react'
-import './myorders.css'
-import {assets} from '../../assets/assets'
+import React, { useState, useContext, useEffect } from 'react'
+import './myOrders.css'
+import { assets } from '../../assets/assets'
 import axios from 'axios';
 import { StoreContext } from '../../Context/StoreContext';
 const myOrders = () => {
 
 
-    const {url,Token} = useContext(StoreContext);
+    const { url, Token } = useContext(StoreContext);
     const [Data, setData] = useState([]);
+    const [LoaderO, setLoaderO] = useState(false);
 
-    const fetchOrders = async ()=>{
-
-        const response = await axios.post(url+"/api/order/userorders",{},{headers:{Token}});
+    const fetchOrders = async () => {
+        setLoaderO(true)
+        const response = await axios.post(url + "/api/order/userorders", {}, { headers: { Token } });
         setData(response.data.data);
         // console.log(response.data.data);
-        
+        setLoaderO(false)
+
     }
 
     useEffect(() => {
-      if(Token){
-        fetchOrders();
-      }
-      
-    }, [Token])
-    
+        if (Token) {
+            fetchOrders();
+        }
 
-  return (
-    <div className='myorders'>
-        <h2>My Orders</h2>
-        <p style={{"margin-top":"15px"}}>Order details also sent on your email</p>
-        <div className="container">
-            {Data.map((order,index)=>{
-                return(
-                    <div key={index} className="my-order-orders">
-                        <img src={assets.parcel_icon} alt="" />
-                        <p>{order.items.map((item,index)=>{
-                            //  return item.name+" X "+item.quantity
-                            if (index === order.items.length-1) {
-                                return item.name+" x "+item.quantity
-                            }
-                            else{
-                                return item.name+" x "+item.quantity+","
-                            }
-                        })}</p>
-                        <p>₹{order.amount}.00</p>
-                        <p>Items: {order.items.length}</p>
-                        
-                        <p><span>&#x25cf;</span><b>{order.status}</b></p>
-                        <button onClick={fetchOrders}>Refresh Order</button>
-                    </div>
-                )
-            })}
+    }, [Token])
+
+
+    return (
+        <div className='myorders'>
+            <h2>My Order's</h2>
+            <p style={{ marginTop: "15px" }}>Once Order is Placed it also sent on your email.</p>
+            <div className="container">
+                {Data.length === 0 && !LoaderO &&<center><p>No orders found.</p></center>}
+                {Data.map((order, index) => {
+                    return (
+                        <>
+                            <div key={index}>
+                                <div className="order-date">
+                                    <p>{order.current_date}</p>
+                                </div>
+                                <div className="my-order-orders">
+                                    <img src={assets.parcel_icon} alt="" />
+                                    <p>{order.items.map((item, index) => {
+                                        //  return item.name+" X "+item.quantity
+                                        if (index === order.items.length - 1) {
+                                            return item.name + " x " + item.quantity
+                                        }
+                                        else {
+                                            return item.name + " x " + item.quantity + ","
+                                        }
+                                    })}</p>
+                                    <p>₹{order.amount}.00</p>
+                                    <p>Items: {order.items.length}</p>
+                                    <p><span>&#x25cf;</span><b>{order.status}</b></p>
+                                    <button onClick={fetchOrders}>Refresh Order</button>
+                                </div>
+                            </div>
+                        </>
+                    )
+                })}
+            </div>
+
         </div>
-      
-    </div>
-  )
+
+    )
 }
 
 export default myOrders
